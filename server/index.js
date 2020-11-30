74a095220d29f73735359b268590a44d33606fa7
Pou Lin <poumason@live.com>
2020-11-30 15:04:03 +0800

    fixed server port bug

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const ogs = require('open-graph-scraper');
 const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 80;
 
 app.use(cors());
 
